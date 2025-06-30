54fa0d2aa6772648b005ee03a5f310258784a359
Digudev <1.5.dg.diego@gmail.com>
2025-06-30 13:15:17 -0600

    Config

--- tailwind.config.lab.js
+++ tailwind.config.lab.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("./utilities/colors");
+const colors = require('./utilities/colors');
 
 module.exports = {
-  content: ["./pages/index-lab.html"],
-  theme: {
-    extend: {
-      colors,
-    },
-  },
-  plugins: [],
+	content: ['./pages/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors
+		}
+	},
+	plugins: []
 };
